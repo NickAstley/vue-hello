9aa8ce3b6f13d2e65ba5f3cd9f8974d41758a5c0
NickAstley <andreanicastro1994@gmail.com>
2022-06-09 15:50:08 +0200

    Completed first task

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const appVue = new Vue({
+    el: "#app",
+    data: {
+        message: "Stampato da Vue!"
+    }
+});
